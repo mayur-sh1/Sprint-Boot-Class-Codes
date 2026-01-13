ac862a05eb1d3c826c2cbdf9cc57e68cd087a99a
mayur-sh1 <mayursharma20000@gmail.com>
2026-01-13 15:07:00 +0530

    Adding Day 1 work with three routes

---
+++ demo/demo/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Hello {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello !, This is my first line";
+    }
+    @GetMapping("/second")
+    public String dusraRoute(){
+        return "This is my second route.";
+    }
+    @GetMapping("/third")
+    public String thirdRoute(){
+        return "This is my third route.";
+    }
+}
